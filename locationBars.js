91062953dee4dd893f813ec1efd827d48405d84a
sergio contrino <contrino.it@gmail.com>
2015-04-14 10:46:19 +0100

    first test commit

--- locationBars.js
+++ locationBars.js
@@ -40,12 +40,10 @@ console.log("BB " + bar)
       .attr("y", barHeight / 2)
       .attr("dy", ".35em")
       // .text(function(d) { return (d[1] - d[0])});
-      .text(function(d) { return (d[0] + " --> " + d[1])});
+      .text(function(d) { return (d[0] + " |--> " + d[1])});
 
 
 });
-
-
 
 
 // d3.json("http://intermine.modencode.org/thalemineval/service/query/results?query=%3Cquery%20model=%22genomic%22%20view=%22Protein.proteinDomainRegions.start%20Protein.proteinDomainRegions.end%20Protein.proteinDomainRegions.originalDb%20Protein.proteinDomainRegions.originalId%20Protein.proteinDomainRegions.proteinDomain.primaryIde\
@@ -84,16 +82,6 @@ console.log("BBB " + x.domain);
       .text(27);
 
 
-
-  // bar.append("rect")
-  //     .attr("width", function(d) { return x(d.value); })
-  //     .attr("height", barHeight - 1);
-
-  // bar.append("text")
-  //     .attr("x", function(d) { return x(d.value) - 3; })
-  //     .attr("y", barHeight / 2)
-  //     .attr("dy", ".35em")
-  //     .text(function(d) { return d.value; });
 });
 
 function type(d) {
@@ -101,58 +89,3 @@ function type(d) {
   return d;
 }
 
-
-
-
-/*
-
-var width = 420,
-    barHeight = 20;
-
-// var x = d3.scale.linear()
-//     .domain([0, d3.max(data)])
-//     .range([0, width]);
-
-// var chart = d3.select(".chart")
-//     .attr("width", width)
-//     .attr("height", barHeight * data.length);
-
-// var bar = chart.selectAll("g")
-//     .data(data)
-//   .enter().append("g")
-//     .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
-
-// NO
-// var data = d3.json("http://intermine.modencode.org/thalemineval/service/query/results?query=%3Cquery%20model=%22genomic%22%20view=%22Protein.proteinDomainRegions.start%20Protein.proteinDomainRegions.end%20Protein.proteinDomainRegions.originalDb%20Protein.proteinDomainRegions.originalId%20Protein.proteinDomainRegions.proteinDomain.primaryIde\
-// ntifier%22%20%3E%20%3Cconstraint%20path=%22Protein.primaryIdentifier%22%20op=%22=%22%20value=%22Q93V56_ARATH%22%20code=%22A%22%20/%3E%20%3C/query%3E");
-// console.log(data);
-
-
-d3.json("http://intermine.modencode.org/thalemineval/service/query/results?query=%3Cquery%20model=%22genomic%22%20view=%22Protein.proteinDomainRegions.start%20Protein.proteinDomainRegions.end%20Protein.proteinDomainRegions.originalDb%20Protein.proteinDomainRegions.originalId%20Protein.proteinDomainRegions.proteinDomain.primaryIde\
-ntifier%22%20%3E%20%3Cconstraint%20path=%22Protein.primaryIdentifier%22%20op=%22=%22%20value=%22Q93V56_ARATH%22%20code=%22A%22%20/%3E%20%3C/query%3E", 
-function(response) {
-console.log(response);
-}
-
-
-//,
-// function(d) {
-// return {
-// begin: d[0],
-// end: d[1]
-// }},
-
-
-// bar.append("rect")
-//     .attr("width", x)
-//     .attr("height", barHeight - 1);
-
-// bar.append("text")
-//     .attr("x", function(d) { return x(d) - 3; })
-//     .attr("y", barHeight / 2)
-//     .attr("dy", ".35em")
-//     .text(function(d) { return d; });
-
-);
-*/
