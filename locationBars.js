0a260f6be31fbf7c060e271708a2959605dfe19f
sergio contrino <contrino.it@gmail.com>
2015-04-20 14:39:22 +0100

    introduces scaling

--- locationBars.js
+++ locationBars.js
@@ -11,7 +11,7 @@
 //
 // ======================
 
-var width = 920,
+var width = 840,
     barHeight = 20;
 
 var x = d3.scale.linear()
@@ -20,23 +20,24 @@ var x = d3.scale.linear()
 var chart = d3.select(".chart")
     .attr("width", width);
 
-// var QUERY = "http://intermine.modencode.org/thalemineval/service/query/results?query=%3Cquery%20model=%22genomic%22%20view=%22Protein.proteinDomainRegions.start%20Protein.proteinDomainRegions.end%20Protein.proteinDomainRegions.originalDb%20Protein.proteinDomainRegions.originalId%20Protein.proteinDomainRegions.proteinDomain.primaryIde\                                                                            
-// ntifier%22%20%3E%20%3Cconstraint%20path=%22Protein.primaryIdentifier%22%20op=%22=%22%20value=%22Q93V56_ARATH%22%20code=%22A%22%20/%3E%20%3C/query%3E"
-var BASEURL= "http://intermine.modencode.org/thalemineval/service/query/results?query=";
 //var QUERY = "http://intermine.modencode.org/thalemineval/service/query/results?query=%3Cquery%20name=%22%22%20model=%22genomic%22%20view=%22Protein.proteinDomainRegions.start%20Protein.proteinDomainRegions.end%22%20longDescription=%22%22%3E%20%3Cconstraint%20path=%22Protein.primaryIdentifier%22%20op=%22=%22%20value=%22Q93V56_ARATH%22/%3E%20%3C/query%3E"
-//    var QUERYSTART = "%3Cquery%20name=%22%22%20model=%22genomic%22%20view=%22Protein.proteinDomainRegions.start%20Protein.proteinDomainRegions.end%22%20longDescription=%22%22%3E%20%3Cconstraint%20path=%22Protein.primaryIdentifier%22%20op=%22=%22%20value=%22";
-    var QUERYSTART = "%3Cquery%20model=%22genomic%22%20view=%22Protein.proteinDomainRegions.start%20Protein.proteinDomainRegions.end%20Protein.proteinDomainRegions.originalDb%20Protein.proteinDomainRegions.originalId%20Protein.proteinDomainRegions.proteinDomain.primaryIdentifier%22%20%3E%20%3Cconstraint%20path=%22Protein.primaryIdentifier%22%20op=%22=%22%20value=%22";
-
-    //var queryId="Q93V56_ARATH";
-    var QUERYEND="%22/%3E%20%3C/query%3E";
+//var QUERYSTART = "%3Cquery%20name=%22%22%20model=%22genomic%22%20view=%22Protein.proteinDomainRegions.start%20Protein.proteinDomainRegions.end%22%20longDescription=%22%22%3E%20%3Cconstraint%20path=%22Protein.primaryIdentifier%22%20op=%22=%22%20value=%22";
 // var queryId ="${QUERYID}";
-var QUERY= BASEURL + QUERYSTART + queryId + QUERYEND;
-
-//var QUERY= BASEURL + QUERYSTART + ID + QUERYEND;
+//var queryId="Q93V56_ARATH";
+    var BASEURL= "http://intermine.modencode.org/thalemineval/service/query/results?query=";
+    var QUERYSTART = "%3Cquery%20model=%22genomic%22%20view=%22Protein.proteinDomainRegions.start%20Protein.proteinDomainRegions.end%20Protein.proteinDomainRegions.originalDb%20Protein.proteinDomainRegions.originalId%20Protein.proteinDomainRegions.proteinDomain.primaryIdentifier%22%20%3E%20%3Cconstraint%20path=%22Protein.primaryIdentifier%22%20op=%22=%22%20value=%22";
+    var QUERYEND="%22/%3E%20%3C/query%3E";
+    var QUERY= BASEURL + QUERYSTART + queryId + QUERYEND;
 
 d3.json(QUERY, function(data) {
 
-x.domain([0, d3.max(data.results, function(d) { return d[1]; })]);
+var maxValue = d3.max(data.results, function(d) { return d[1]; });
+var minValue = d3.min(data.results, function(d) { return d[0]; });
+var scaling = width / [maxValue - minValue];
+
+console.log("AAA " + minValue + "--" + maxValue+ "++" + scaling);
+
+x.domain([0, width]);
 
   chart.attr("height", barHeight * data.results.length);
 
@@ -44,11 +45,13 @@ x.domain([0, d3.max(data.results, function(d) { return d[1]; })]);
       .data(data.results)
       .enter().append("g")
       // .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-      .attr("transform", function(d, i) { return "translate("+ d[0] + "," + i * barHeight + ")"; });
+      // .attr("transform", function(d, i) { return "translate("+ d[0] + "," + i * barHeight + ")"; });
+      .attr("transform", function(d, i) { return "translate("+ ((d[0] - minValue)*scaling) + "," + i * barHeight + ")"; });
 
  bar.append("rect")
-      .attr("width", function(d) { return (d[1] -d[0]) })
       //.attr("width", end - begin)
+      // .attr("width", function(d) { return (d[1] -d[0]) })
+      .attr("width", function(d) { return ((d[1] -d[0])*scaling) })
       .attr("height", barHeight - 1);
 
   bar.append("text")
